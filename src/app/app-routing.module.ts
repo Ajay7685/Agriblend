ca65d35cabd20516c29e51517cd55e06c351b33a
Amit Kumar <amitaitgzb2013@gmail.com>
2024-05-21 15:17:54 +0530

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { MiddleComponent } from './core/components/middle/middle.component';
 
 const routes: Routes = [
-  { path: 'home', component: MiddleComponent,title: 'Home | AgriBlend ', },
+  { path: 'home', component: MiddleComponent, title: 'Home | AgriBlend ', },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-
-  
   { path: '**', redirectTo: '/home' } // Wildcard route for a 404 page
 ];
 
