dc4a56e6a42c5355eee9203897fd9a77d5b5fad6
Amit Kumar <amitaitgzb2013@gmail.com>
2024-05-21 15:19:52 +0530

    do the blank path of middle components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MiddleComponent } from './core/components/middle/middle.component';
 
 const routes: Routes = [
-  { path: 'home', component: MiddleComponent, title: 'Home | AgriBlend ', },
+  { path: '', component: MiddleComponent, title: 'Home | AgriBlend ', },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home' } // Wildcard route for a 404 page
 ];
